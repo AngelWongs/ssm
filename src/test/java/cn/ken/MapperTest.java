6ad30b8704447f767b505d1d7acae2c8794ed3e0
AngelWongs <38316188+AngelWongs@users.noreply.github.com>
2019-04-24 10:49:59 +0800

    小bug加版本更新

--- src/test/java/cn/ken/MapperTest.java
+++ src/test/java/cn/ken/MapperTest.java
@@ -3,10 +3,15 @@ package cn.ken;
 import cn.ken.domain.User;
 import cn.ken.mapper.UserMapper;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/applicationContext.xml")
 public class MapperTest extends BaseTest {
     @Autowired
     private UserMapper userMapper;
